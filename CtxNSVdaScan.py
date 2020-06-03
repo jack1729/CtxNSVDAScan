52f4de93480b4568eed746d25a82704aecb10d04
jack1729 <jack1729@users.noreply.github.com>
2020-06-03 17:10:54 -0400

    Update CtxNSVdaScan.py
    adding import statement fixing syntax

--- CtxNSVdaScan.py
+++ CtxNSVdaScan.py
@@ -1,10 +1,17 @@
+import socket
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = sock.connect_ex(('10.14.10.216',1494))
 
 for intThrOctet in range(1,255):
-  CtxUp ('10.14.10.'+intThrOctet)
+  CtxUp('10.14.10.'+intThrOctet)
   
-def CtxUp ( CtxHost )
+def CtxUp( CtxHost ):
+  try: 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    print "Socket successfully created"
+  except socket.error as err: 
+    print "socket creation failed with error %s" %(err) 
+    
   result = sock.connect_ex(('10.14.10.216',1494))
   if result == 0:
         print "Port is open"
